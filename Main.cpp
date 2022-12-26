539ca1e788cacd01c624088b0ec42d9384391e32
Hyunji Kim <108276366+de-quei@users.noreply.github.com>
2022-12-26 14:29:44 +0900

    방향키 동시동작

--- Main.cpp
+++ Main.cpp
@@ -5,10 +5,13 @@ using namespace sf;
 int main(void) {
 	
 	RenderWindow window(VideoMode(640, 480), "AfterSchool"); // 윈도우 창 생성
+	window.setFramerateLimit(60);
 
 	RectangleShape player;
 	player.setSize(Vector2f(40, 40));
 	player.setPosition(100, 100);
+	player.setFillColor(Color::Red);
+	int player_speed = 5;
 
 	while (window.isOpen()) // 윈도우가 열려 있을 때 까지 반복
 	{ 
@@ -22,6 +25,25 @@ int main(void) {
 			}
 			
 		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Left)) 
+		{
+			player.move(-player_speed, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			player.move(player_speed, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+		{
+			player.move(0, -player_speed);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down))
+		{
+			player.move(0, player_speed);
+		}
+
+		window.clear(Color::Black);
 		window.draw(player);
 		window.display();
 	}
